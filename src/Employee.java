e87e4b8fac2eeb013f6a951702f96c64fffeee77
Boris Trivić <trivicb@lanaco.com>
2017-11-23 14:54:07 +0100

    Version 1

---
+++ src/Employee.java
@@ -0,0 +1,19 @@
+class Employee{
+		private String name;
+		private String birth;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getBirth() {
+			return birth;
+		}
+		public void setBirth(String birth) {
+			this.birth = birth;
+		}
+		
+		
+	}
